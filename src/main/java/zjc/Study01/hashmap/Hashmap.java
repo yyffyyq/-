e2a54ffe1811ea6c1b404d28f75e98c507eb3c0b
yyffyyq <35894333@qq.com>
2026-03-21 13:13:01 +0800

    哈希表相关操作：  力扣测试题：   LRU缓存机制制作，用来双向链表加哈希表

--- src/main/java/zjc/Study01/hashmap/Hashmap.java
+++ src/main/java/zjc/Study01/hashmap/Hashmap.java
@@ -5,15 +5,17 @@ import java.util.HashMap;
 public class Hashmap {
     static class LRUCache {
         int capacity;
-        HashMap<Integer,Node> map;
+        HashMap<Integer, Node> map;
         Node head;
         Node tail;
-        class Node{
+
+        class Node {
             int key;
             int data;
             Node prev;
             Node next;
-            public Node(int key,int data){
+
+            public Node(int key, int data) {
                 this.key = key;
                 this.data = data;
             }
@@ -22,8 +24,8 @@ public class Hashmap {
         public LRUCache(int capacity) {
             this.capacity = capacity;
             this.map = new HashMap<>();
-            this.head = new Node(-1,-1);
-            this.tail = new Node(-1,-1);
+            this.head = new Node(-1, -1);
+            this.tail = new Node(-1, -1);
 
             this.head.next = this.tail;
             this.tail.prev = this.head;
@@ -32,7 +34,7 @@ public class Hashmap {
 
         public int get(int key) {
             Node currentNode = this.map.get(key);
-            if(currentNode == null){
+            if (currentNode == null) {
                 return -1;
             }
             currentNode.prev.next = currentNode.next;
@@ -47,7 +49,7 @@ public class Hashmap {
         public void put(int key, int value) {
             Node currentNode = this.map.get(key);
             /// 先判断是否在hash表中有这个key
-            if(currentNode != null){
+            if (currentNode != null) {
                 currentNode.data = value;
                 currentNode.prev.next = currentNode.next;
                 currentNode.next.prev = currentNode.prev;
@@ -59,19 +61,19 @@ public class Hashmap {
             }
             // 判断capacity大小是否超了
             Node readyEleteNode = this.tail.prev;
-            if(this.map.size()>=this.capacity){
+            if (this.map.size() >= this.capacity) {
                 this.map.remove(readyEleteNode.key);
                 readyEleteNode.prev.next = this.tail;
                 this.tail.prev = readyEleteNode.prev;
             }
-            Node addNode = new Node(key,value);
-            this.map.put(key,addNode);
+            Node addNode = new Node(key, value);
+            this.map.put(key, addNode);
             addNode.next = this.head.next;
             addNode.prev = this.head;
             this.head.next.prev = addNode;
             this.head.next = addNode;
         }
-
+    }
 
 /**
  * Your LRUCache object will be instantiated and called as such:
